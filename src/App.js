6ac44e0bc9e30b729eb5e53d850ececf8aefe572
Nodir-Bek <nodirjonabdumansurov@gmail.com>
2021-08-15 10:36:39 +0500

    files created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeScreen from "./containers/HomeScreen";
+import "./App.css";
+const App = () => {
+  return (
+    <div className="App">
+      <HomeScreen />
+    </div>
+  );
+};
+
+export default App;
